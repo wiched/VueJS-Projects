fd9942c5147995bf15f53ae13e52555c2cc55d31
Wiched <wiched@users.noreply.github.com>
2020-04-19 16:18:15 +0300

    Mastering Vuex
    first two videos

---
+++ 03. Mastering Vuex/real-world-vue-lesson11-vuex-start/src/store.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ]
+  },
+  mutations: {},
+  actions: {},
+  getters: {
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
+})
---
+++ 03. Mastering Vuex/real-world-vue-lesson11-vuex-start/src/views/EventCreate.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Create an Event, {{user.name}}</h1>
+    <p>This event was created by {{user.id}}</p>
+    <p>{{getEventById(2)}}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{cat}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters(['getEventById']),
+    ...mapState(['user', 'categories'])
+  }
+}
+</script>
+
+<style  scoped>
+</style>
