3babd8aa89eddf82b79395d47ac5f8b5f1e6f6cd
Wiched <wiched@users.noreply.github.com>
2020-05-02 13:22:51 +0300

    communication between components assignment

---
+++ Vue JS 2 - The Complete Guide (incl. Vue Router & Vuex)/08. Communicating between Components/assignment/src/components/Server/Server.vue
@@ -0,0 +1,22 @@
+<template>
+  <li
+    class="list-group-item"
+    @click="serverSelected"
+    style="cursor: pointer"
+  >Server #{{ server.id }}</li>
+</template>
+
+<script>
+import { serverBus } from "../../main";
+export default {
+  props: ["server"],
+  methods: {
+    serverSelected() {
+      serverBus.$emit("serverSelected", this.server);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- Vue JS 2 - The Complete Guide (incl. Vue Router & Vuex)/08. Communicating between Components/assignment/src/components/Server/ServerDetails.vue
+++ Vue JS 2 - The Complete Guide (incl. Vue Router & Vuex)/08. Communicating between Components/assignment/src/components/Server/ServerDetails.vue
@@ -1,13 +1,32 @@
 <template>
-    <div class="col-xs-12 col-sm-6">
-        <p>Server Details are currently not updated</p>
-    </div>
-
+  <div class="col-xs-12 col-sm-6">
+    <p v-if="!server">Please select a server</p>
+    <p v-else>Server {{server.id}} selected, status: {{server.status}}</p>
+    <hr />
+    <button @click="resetStatus">Change to Normal</button>
+  </div>
 </template>
 
 <script>
+import { serverBus } from "../../main";
+export default {
+  data() {
+    return {
+      server: null
+    };
+  },
+  methods: {
+    resetStatus() {
+      this.server.status = "Normal";
+    }
+  },
+  created() {
+    serverBus.$on("serverSelected", server => {
+      this.server = server;
+    });
+  }
+};
 </script>
 
 <style>
-
 </style>
